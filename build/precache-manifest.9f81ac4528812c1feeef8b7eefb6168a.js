e6a779fc9d085a2264f94d4295c2e196c19aa903
unknown <zhaoyingzi>
2019-07-17 23:30:31 +0800

    fix

---
+++ build/precache-manifest.9f81ac4528812c1feeef8b7eefb6168a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686a4a0d9cb92869ad94a895b3306d7f",
+    "url": "/aliIndex/index.html"
+  },
+  {
+    "revision": "c0517f390214f94607c3",
+    "url": "/aliIndex/static/css/main.996652e8.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/aliIndex/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "c0517f390214f94607c3",
+    "url": "/aliIndex/static/js/main.bb14270e.chunk.js"
+  },
+  {
+    "revision": "f93e671a971bbc6c64af",
+    "url": "/aliIndex/static/js/runtime~main.41da8910.js"
+  }
+]);
